ded76aa8e603096511c7d423f26130bf8bad3d88
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2025-11-12 21:01:33 +0300

    remove old code

--- projects/react-examples/src/App.tsx
+++ projects/react-examples/src/App.tsx
@@ -1,11 +1,8 @@
-import { Button } from 'reformer';
-
 function App() {
   return (
     <>
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
-        <Button>Click me</Button>
+        <h1 className="text-3xl font-bold underline mb-4">Credit form</h1>
       </div>
     </>
   );
