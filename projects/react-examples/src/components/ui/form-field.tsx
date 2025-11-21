c02672d25ffb10e65e5f1c9ddca2c053ba968d30
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2025-11-21 22:03:51 +0300

    refactor: use useFormControl in form field

--- projects/react-examples/src/components/ui/form-field.tsx
+++ projects/react-examples/src/components/ui/form-field.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type { FieldNode } from 'reformer';
+import { useFormControl, type FieldNode } from 'reformer';
 import { Checkbox } from './checkbox';
 
 export interface FormFieldProps {
@@ -9,13 +9,12 @@ export interface FormFieldProps {
 }
 
 const FormFieldComponent: React.FC<FormFieldProps> = ({ control, className }) => {
-  // TODO: Рассмотреть вариант использовать hook как средство изоляции от сигналов
-  // const { value, errors, pending, disabled } = useFormControl(control);
-  // console.log('[useFormControl]: ', value, errors, pending, disabled)
+  const { value, errors, pending, disabled, invalid, shouldShowError } = useFormControl(control);
+
   const Component = control.component;
-  // Конвертируем null/undefined в безопасные значения
   const isCheckbox = control.component === Checkbox;
-  const safeValue = control.value.value ?? (isCheckbox ? false : '');
+  // Конвертируем null/undefined в безопасные значения
+  const safeValue = value.value ?? (isCheckbox ? false : '');
 
   return (
     <div className={className}>
@@ -42,16 +41,16 @@ const FormFieldComponent: React.FC<FormFieldProps> = ({ control, className }) =>
             control.validate();
           }
         }}
-        disabled={control.status.value === 'disabled'}
-        aria-invalid={control.invalid.value}
+        disabled={disabled.value}
+        aria-invalid={invalid.value}
         {...control.componentProps.value}
       />
 
-      {control.shouldShowError.value && (
-        <span className="text-red-500 text-sm mt-1 block">{control.errors.value[0]?.message}</span>
+      {shouldShowError.value && (
+        <span className="text-red-500 text-sm mt-1 block">{errors.value[0]?.message}</span>
       )}
 
-      {control.pending.value && (
+      {pending.value && (
         <span className="text-gray-500 text-sm mt-1 block">Проверка...</span>
       )}
     </div>
