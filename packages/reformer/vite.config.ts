8869bd66f427bfd37c844c8b41fe43adf0441ce2
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2025-11-30 16:51:43 +0300

    fix: build

--- packages/reformer/vite.config.ts
+++ packages/reformer/vite.config.ts
@@ -3,18 +3,24 @@
 
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [dts({ insertTypesEntry: true })],
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'Reformer',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`,
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+        validators: resolve(__dirname, 'src/core/validation/index.ts'),
+        behaviors: resolve(__dirname, 'src/core/behavior/index.ts'),
+      },
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'uuid'],
+      output: {
+        entryFileNames: '[name].js',
+      },
     },
   },
 });
